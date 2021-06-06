954d4c22f1bdde90a91903162148227ee89a6205
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2021-06-06 21:07:38 +0200

    Replace nuxt icon with emoji

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌵</text></svg>' }
     ]
   },
 
