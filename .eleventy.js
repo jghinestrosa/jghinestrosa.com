021c72551a150c703a5fe67c5bfece3feee90219
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2026-07-06 21:57:11 +0200

    Remove liquid and migrate everything to njk

--- .eleventy.js
+++ .eleventy.js
@@ -44,16 +44,15 @@ module.exports = function(eleventyConfig) {
 
 	return {
 		// Control which files Eleventy will process
-		// e.g.: *.md, *.njk, *.html, *.liquid
+		// e.g.: *.md, *.njk, *.html
 		templateFormats: [
 			"md",
 			"njk",
 			"html",
-			"liquid",
 		],
 
 		// Pre-process *.md files with: (default: `liquid`)
-		//markdownTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
 
 		// Pre-process *.html files with: (default: `liquid`)
 		//htmlTemplateEngine: "njk",
